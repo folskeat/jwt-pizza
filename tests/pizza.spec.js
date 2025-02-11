6ecc684ca8352c761d7c85bc5eac73779d94feab
Keaton Folsom <89952496+folskeat@users.noreply.github.com>
2025-02-11 09:37:24 -0700

    playwright stuff

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -120,3 +120,21 @@ test("purchase with login", async ({ page }) => {
   // Check balance
   await expect(page.getByText("0.008")).toBeVisible();
 });
+test("register", async ({ page }) => {
+  //LET'S MAKE THIS INTO A MEANINGFUL TEST!!!!
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Register" }).click();
+  await page.getByRole("textbox", { name: "Full name" }).fill("joe");
+  await page.getByRole("textbox", { name: "Full name" }).press("Tab");
+  await page
+    .getByRole("textbox", { name: "Email address" })
+    .fill("joe@email.com");
+  await page.getByRole("textbox", { name: "Email address" }).press("Tab");
+  await page.getByRole("textbox", { name: "Password" }).fill("password");
+  await page.getByRole("button", { name: "Register" }).click();
+  await page.getByRole("textbox", { name: "Full name" }).click();
+});
+
+test("create franchise", async ({ page }) => {});
+
+test("create store", async ({ page }) => {});
